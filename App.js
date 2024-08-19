e6af328ac5687a88ff3d93b82c6c2dfa3f9d38b2
Gab2270 <142789170+Gab2270@users.noreply.github.com>
2024-08-19 10:52:26 -0300

    ytry

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import TelaJavaScript from "./componentes/TelaJavaScript";
+import TelaReact from "./componentes/TelaReact";
+import TelaNode from "./componentes/TelaNode";
+
+const MenuNav = createStackNavigator();
 
 export default function App() {
   return (
-
+   <NavigationContainer>
+    <MenuNav.Navigator>
+      <MenuNav.Screen name="Home" component={ TelaJavaScript } />
+      <MenuNav.Screen name="Node" component={ TelaNode } />
+      <MenuNav.Screen name="React" component={ TelaReact } />
+    </MenuNav.Navigator>
+    </NavigationContainer>
   );
 }
